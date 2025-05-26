289e1d95035b4d8858f2690939173a95140591ba
Adrian Gonzalez Astudillo <adrian.gonzalezjaramillo@gmail.com>
2025-05-26 00:21:52 -0500

    AJUSTE DE RUTAS Y METODOS

--- api-backend/src/controller/usuario_controller.js
+++ api-backend/src/controller/usuario_controller.js
@@ -2,95 +2,6 @@ import { db_pool_connection } from "../database/db.js";
 import { response_success, response_create, response_not_found, response_error, response_bad_request } from "../response/responses.js";
 
 // USUARIO
-export const IngresarUsuario = async (req, res) => {
-    try {
-
-        const { username, password, email, rol } = req.body;  
-        const query = `
-                INSERT INTO usuario 
-                (username, password, email, rol)
-                VALUES (?, ?, ?, ?)
-            `;
-
-        const [rows] = await db_pool_connection.query(query, [username, password, email, rol]);
-        console.log("INSERT USER: ", rows);
-        res.status(201).json(response_create(rows.insertId, "USUARIO REGISTRADO CON EXITO"));
-
-    } catch (error) {
-        console.error("ERROR: ", error);
-
-        if (error.code === 'ER_DUP_ENTRY') {
-            if (error.sqlMessage.includes('username')) {
-                return res.status(409).json(response_bad_request("USERNAME YA REGISTRADO"));
-            }
-            if (error.sqlMessage.includes('email')) {
-                return res.status(409).json(response_bad_request("EMAIL YA REGISTRADO"));
-            }
-        }
-
-        res.status(500).json(response_error("ERROR API-SQL -> " + error['sqlMessage']));
-    }
-}
-
-export const ActualizarUsuario = async (req, res) => {
-    try {
-
-        const { id } = req.body;
-        const { username, password, email, rol } = req.body;
-        const query = `
-            UPDATE usuario
-            SET username = ?, password = ?, email = ?, rol = ?
-            WHERE id = ?
-        `;
-
-        const [rows] = await db_pool_connection.query(query, [username, password, email, rol, id]);
-        if (rows.affectedRows === 0) {
-            return res.status(404).json(response_not_found("USUARIO NO ENCONTRADO"));
-        } else {
-            console.log("UPDATE USER: ", rows);
-            res.status(200).json(response_success(null, "USUARIO ACTUALIZADO CON EXITO"));
-        }
-
-    } catch (error) {
-        console.error("ERROR: ", error);
-
-        if (error.code === 'ER_DUP_ENTRY') {
-            if (error.sqlMessage.includes('username')) {
-                return res.status(409).json(response_bad_request("USERNAME YA REGISTRADO"));
-            }
-            if (error.sqlMessage.includes('email')) {
-                return res.status(409).json(response_bad_request("EMAIL YA REGISTRADO"));
-            }
-        }
-
-        res.status(500).json(response_error("ERROR API-SQL -> " + error['sqlMessage']));
-    }
-}
-
-export const EstadoUsuario = async (req, res) => {
-    try {
-
-        const { id, estado } = req.body;
-        const query = `
-            UPDATE usuario
-            SET estado = ?
-            WHERE id = ?
-        `;
-
-        const [rows] = await db_pool_connection.query(query, [estado, id]);
-        if (rows.affectedRows === 0) {
-            return res.status(404).json(response_not_found("USUARIO NO ENCONTRADO"));
-        } else {
-            console.log("STATUS USER: ", rows);
-            res.status(200).json(response_success(null, "ESTADO DE USUARIO ACTUALIZADO"));
-        }
-
-    } catch (error) {
-        console.error("ERROR: ", error);
-        res.status(500).json(response_error("ERROR API-SQL -> " + error['sqlMessage']));
-    }
-}
-
 export const Login = async (req, res) => {
     try {
 
@@ -115,22 +26,284 @@ export const Login = async (req, res) => {
     }
 }
 
-export const ConsultarUsuario = async (req, res) => {
+export const IngresarUsuarioTecnico = async (req, res) => {
+    const connection = await db_pool_connection.getConnection();
+
+    try {
+
+        await connection.beginTransaction();
+        const { username, password, email, rol } = req.body;
+        const { cedula, nombres, apellidos, celular } = req.body;
+
+        const queryUsuario = `
+            INSERT INTO usuario (username, password, email, rol)
+            VALUES (?, ?, ?, ?)
+        `;
+
+        const [usuarioResult] = await connection.query(queryUsuario, [username, password, email, rol]);
+        const id_usuario = usuarioResult.insertId;
+
+        const queryTecnico = `
+            INSERT INTO tecnico (cedula, nombres, apellidos, celular, id_usuario)
+            VALUES (?, ?, ?, ?, ?)
+        `;
+
+        const [tecnicoResult] = await connection.query(queryTecnico, [cedula, nombres, apellidos, celular, id_usuario]);
+        await connection.commit();
+        res.status(201).json(response_create(tecnicoResult.insertId, "USUARIO Y TECNICO REGISTRADOS CON ÉXITO"));
+
+    } catch (error) {
+        await connection.rollback();
+        console.error("ERROR: ", error);
+
+        if (error.code === 'ER_DUP_ENTRY') {
+            if (error.sqlMessage.includes('username')) {
+                return res.status(409).json(response_bad_request("USERNAME YA REGISTRADO"));
+            }
+            if (error.sqlMessage.includes('email')) {
+                return res.status(409).json(response_bad_request("EMAIL YA REGISTRADO"));
+            }
+            if (error.sqlMessage.includes('cedula')) {
+                return res.status(409).json(response_bad_request("CEDULA YA REGISTRADA"));
+            }
+        }
+
+        res.status(500).json(response_error("ERROR API-SQL -> " + error.sqlMessage));
+    } finally {
+        connection.release();
+    }
+};
+
+export const IngresarUsuarioCliente = async (req, res) => {
+    const connection = await db_pool_connection.getConnection(); 
+
+    try {
+
+        await connection.beginTransaction();
+        const { username, password, email, rol } = req.body;
+        const { cedula, nombres, apellidos, celular, direccion } = req.body;
+
+        const queryUsuario = `
+            INSERT INTO usuario (username, password, email, rol)
+            VALUES (?, ?, ?, ?)
+        `;
+
+        const [usuarioResult] = await connection.query(queryUsuario, [username, password, email, rol]);
+        const id_usuario = usuarioResult.insertId;
+
+        const queryCliente = `
+            INSERT INTO cliente (cedula, nombres, apellidos, celular, direccion, id_usuario)
+            VALUES (?, ?, ?, ?, ?, ?)
+        `;
+
+        const [clienteResult] = await connection.query(queryCliente, [cedula, nombres, apellidos, celular, direccion, id_usuario]);
+        await connection.commit();
+        res.status(201).json(response_create(clienteResult.insertId, "USUARIO Y CLIENTE REGISTRADOS CON ÉXITO"));
+
+    } catch (error) {
+        await connection.rollback();
+        console.error("ERROR: ", error);
+
+        if (error.code === 'ER_DUP_ENTRY') {
+            if (error.sqlMessage.includes('username')) {
+                return res.status(409).json(response_bad_request("USERNAME YA REGISTRADO"));
+            }
+            if (error.sqlMessage.includes('email')) {
+                return res.status(409).json(response_bad_request("EMAIL YA REGISTRADO"));
+            }
+            if (error.sqlMessage.includes('cedula')) {
+                return res.status(409).json(response_bad_request("CEDULA YA REGISTRADA"));
+            }
+        }
+
+        res.status(500).json(response_error("ERROR API-SQL -> " + error.sqlMessage));
+    } finally {
+        connection.release();
+    }
+}
+
+export const ConsultarUsuarioTecnico = async (req, res) => {
     try {
 
         const { id } = req.body;
         const query = `
-            SELECT username, password, email
-            FROM usuario
-            WHERE id = ?
+            SELECT 
+                u.username, u.password, u.email,
+                t.cedula, t.nombres, t.apellidos, t.celular
+            FROM usuario u
+            INNER JOIN tecnico t ON u.id = t.id_usuario
+            WHERE u.id = ?
         `;
         
         const [rows] = await db_pool_connection.query(query, [id]);
         if (rows.length <= 0) {
+            return res.status(404).json(response_not_found("USUARIO TECNICO NO ENCONTRADO"));
+        } else {
+            console.log("SELECT TEC: ", rows);
+            res.status(200).json(response_success(rows, "CONSULTA EXITOSA"));
+        }
+
+    } catch (error) {
+        console.error("ERROR: ", error);
+        res.status(500).json(response_error("ERROR API-SQL -> " + error['sqlMessage']));
+    }
+}
+
+export const ConsultarUsuarioCliente = async (req, res) => {
+    try {
+
+        const { id } = req.body;
+        const query = `
+            SELECT 
+                u.username, u.password, u.email,
+                c.cedula, c.nombres, c.apellidos, c.celular, c.direccion
+            FROM usuario u
+            INNER JOIN cliente c ON u.id = c.id_usuario
+            WHERE u.id = ?
+        `;
+        
+        const [rows] = await db_pool_connection.query(query, [id]);
+        if (rows.length <= 0) {
+            return res.status(404).json(response_not_found("USUARIO CLIENTE NO ENCONTRADO"));
+        } else {
+            console.log("SELECT TEC: ", rows);
+            res.status(200).json(response_success(rows, "CONSULTA EXITOSA"));
+        }
+
+    } catch (error) {
+        console.error("ERROR: ", error);
+        res.status(500).json(response_error("ERROR API-SQL -> " + error['sqlMessage']));
+    }
+}
+
+export const ActualizarUsuarioTecnico = async (req, res) => {
+    const connection = await db_pool_connection.getConnection();
+
+    try {
+        await connection.beginTransaction();
+        const { id_usuario, username, password, email } = req.body;
+        const { cedula, nombres, apellidos, celular } = req.body;
+
+        const queryUsuario = `
+            UPDATE usuario
+            SET username = ?, password = ?, email = ?
+            WHERE id = ?
+        `;
+
+        const [usuarioResult] = await connection.query(queryUsuario, [username, password, email, id_usuario]);
+        if (usuarioResult.affectedRows === 0) {
+            await connection.rollback();
+            return res.status(404).json(response_not_found("USUARIO NO ENCONTRADO"));
+        }
+
+        const queryTecnico = `
+            UPDATE tecnico
+            SET cedula = ?, nombres = ?, apellidos = ?, celular = ?
+            WHERE id_usuario = ?
+        `;
+
+        const [tecnicoResult] = await connection.query(queryTecnico, [cedula, nombres, apellidos, celular, id_usuario]);
+        if (tecnicoResult.affectedRows === 0) {
+            await connection.rollback();
+            return res.status(404).json(response_not_found("TECNICO NO ENCONTRADO"));
+        }
+        await connection.commit();
+        res.status(200).json(response_success(null, "USUARIO Y TECNICO ACTUALIZADOS CON ÉXITO"));
+
+    } catch (error) {
+        await connection.rollback();
+        console.error("ERROR: ", error);
+
+        if (error.code === 'ER_DUP_ENTRY') {
+            if (error.sqlMessage.includes('username')) {
+                return res.status(409).json(response_bad_request("USERNAME YA REGISTRADO"));
+            }
+            if (error.sqlMessage.includes('email')) {
+                return res.status(409).json(response_bad_request("EMAIL YA REGISTRADO"));
+            }
+            if (error.sqlMessage.includes('cedula')) {
+                return res.status(409).json(response_bad_request("CEDULA YA REGISTRADA"));
+            }
+        }
+
+        res.status(500).json(response_error("ERROR API-SQL -> " + error.sqlMessage));
+    } finally {
+        connection.release();
+    }
+};
+
+export const ActualizarUsuarioCliente = async (req, res) => {
+    const connection = await db_pool_connection.getConnection();
+
+    try {
+        await connection.beginTransaction();
+        const { id_usuario, username, password, email } = req.body;
+        const { cedula, nombres, apellidos, celular, direccion } = req.body;
+
+        const queryUsuario = `
+            UPDATE usuario
+            SET username = ?, password = ?, email = ?
+            WHERE id = ?
+        `;
+
+        const [usuarioResult] = await connection.query(queryUsuario, [username, password, email, id_usuario]);
+        if (usuarioResult.affectedRows === 0) {
+            await connection.rollback();
+            return res.status(404).json(response_not_found("USUARIO NO ENCONTRADO"));
+        }
+
+        const queryCliente = `
+            UPDATE cliente
+            SET cedula = ?, nombres = ?, apellidos = ?, celular = ?, direccion = ?
+            WHERE id_usuario = ?
+        `;
+
+        const [clienteResult] = await connection.query(queryCliente, [cedula, nombres, apellidos, celular, direccion, id_usuario]);
+        if (clienteResult.affectedRows === 0) {
+            await connection.rollback();
+            return res.status(404).json(response_not_found("CLIENTE NO ENCONTRADO"));
+        }
+        await connection.commit();
+        res.status(200).json(response_success(null, "USUARIO Y CLIENTE ACTUALIZADOS CON ÉXITO"));
+
+    } catch (error) {
+        await connection.rollback();
+        console.error("ERROR: ", error);
+
+        if (error.code === 'ER_DUP_ENTRY') {
+            if (error.sqlMessage.includes('username')) {
+                return res.status(409).json(response_bad_request("USERNAME YA REGISTRADO"));
+            }
+            if (error.sqlMessage.includes('email')) {
+                return res.status(409).json(response_bad_request("EMAIL YA REGISTRADO"));
+            }
+            if (error.sqlMessage.includes('cedula')) {
+                return res.status(409).json(response_bad_request("CEDULA YA REGISTRADA"));
+            }
+        }
+
+        res.status(500).json(response_error("ERROR API-SQL -> " + error.sqlMessage));
+    } finally {
+        connection.release();
+    }
+};
+
+export const EstadoUsuario = async (req, res) => {
+    try {
+
+        const { id, estado } = req.body;
+        const query = `
+            UPDATE usuario
+            SET estado = ?
+            WHERE id = ?
+        `;
+
+        const [rows] = await db_pool_connection.query(query, [estado, id]);
+        if (rows.affectedRows === 0) {
             return res.status(404).json(response_not_found("USUARIO NO ENCONTRADO"));
         } else {
-            console.log("SELECT USER: ", rows);
-            res.status(200).json(response_success(rows, "CONSULTA EXITOSA"));
+            console.log("STATUS USER: ", rows);
+            res.status(200).json(response_success(null, "ESTADO DE USUARIO ACTUALIZADO"));
         }
 
     } catch (error) {
--- api-backend/src/route/reparacion_routes.js
+++ api-backend/src/route/reparacion_routes.js
@@ -1,31 +1,28 @@
 import { Router } from "express";
-import { ActualizarCliente, ConsultarCliente, IngresarCliente, ListarCliente } from "../controller/cliente_controller.js";
+import { ListarCliente } from "../controller/cliente_controller.js";
 import { IngresarFactura } from "../controller/factura_controller.js";
 import { CancelarReparacion, HistorialReparacion, IngresarReparacion, ListarReparacionDisponible } from "../controller/reparacion_controller.js";
 import { IngresarReparacionRepuesto } from "../controller/reparacion_repuesto_controller.js";
 import { IngresarReparacionTecnico } from "../controller/reparacion_tecnico_controller.js";
 import { ActualizarRepuesto, EstadoRepuesto, IngresarRepuesto, ListarRepuesto, StockRepuesto } from "../controller/repuesto_controller.js";
-import { ActualizarTecnico, ConsultarTecnico, IngresarTecnico, ListarTecnico } from "../controller/tecnico_controller.js";
-import { ActualizarUsuario, ConsultarUsuario, EstadoUsuario, IngresarUsuario, Login } from "../controller/usuario_controller.js";
+import { ListarTecnico } from "../controller/tecnico_controller.js";
+import { ActualizarUsuarioCliente, ActualizarUsuarioTecnico, ConsultarUsuarioCliente, ConsultarUsuarioTecnico, EstadoUsuario, IngresarUsuarioCliente, IngresarUsuarioTecnico, Login } from "../controller/usuario_controller.js";
 
 const rutas_reparacion = Router();
 
 // USUARIO
 rutas_reparacion.post("/repair/login", Login);
-rutas_reparacion.post("/repair/usuario/register", IngresarUsuario);
-rutas_reparacion.get("/repair/usuario", ConsultarUsuario);
-rutas_reparacion.put("/repair/usuario", ActualizarUsuario);
+rutas_reparacion.post("/repair/usuario/register/tecnico", IngresarUsuarioTecnico);
+rutas_reparacion.post("/repair/usuario/register/cliente", IngresarUsuarioCliente);
+rutas_reparacion.get("/repair/usuario/tecnico", ConsultarUsuarioTecnico);
+rutas_reparacion.get("/repair/usuario/cliente", ConsultarUsuarioCliente);
+rutas_reparacion.put("/repair/usuario/tecnico", ActualizarUsuarioTecnico);
+rutas_reparacion.put("/repair/usuario/cliente", ActualizarUsuarioCliente);
 rutas_reparacion.put("/repair/usuario/status", EstadoUsuario);
 // TECNICO
-rutas_reparacion.post("/repair/usuario/register/tecnico", IngresarTecnico);
-rutas_reparacion.get("/repair/usuario/tecnico", ConsultarTecnico);
 rutas_reparacion.get("/repair/usuario/tecnico/list", ListarTecnico);
-rutas_reparacion.put("/repair/usuario/tecnico", ActualizarTecnico);
 // CLIENTE
-rutas_reparacion.post("/repair/usuario/register/cliente", IngresarCliente);
-rutas_reparacion.get("/repair/usuario/cliente", ConsultarCliente);
 rutas_reparacion.get("/repair/usuario/cliente/list", ListarCliente);
-rutas_reparacion.put("/repair/usuario/cliente", ActualizarCliente);
 // REPUESTO
 rutas_reparacion.post("/repair/repuesto", IngresarRepuesto);
 rutas_reparacion.get("/repair/repuesto", ListarRepuesto);
